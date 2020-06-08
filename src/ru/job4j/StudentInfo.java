a4c90221a5b70e77934e21fe72df366a2388259f
Vasya BelovKulagin1987git config --global user.name Vasya <unserfuhrer@list.ru>
2020-06-08 17:11:39 +0300

    3.3. Разбор первой программы. [#301273]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ivan  Kulagin");
+        System.out.print("24.04.1987");
+    }
+}
