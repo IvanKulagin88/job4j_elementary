c1629ddddd4eadec67fe89b7a5edea5e6da5a399
Vasya BelovKulagin1987git config --global user.name Vasya <unserfuhrer@list.ru>
2020-06-09 18:27:35 +0300

    0. Что такое тестирование [#301290]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -15,6 +15,17 @@ public class Converter {
         int euro = Converter.rubleToEuro(140);
         int dollar = Converter.rubleToDollar(180);
         System.out.println("140 rubles are " + euro + " euro.");
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
         System.out.println("180 rubles are " + dollar + " dollar.");
+        in = 180;
+         expected = 3;
+         out = rubleToDollar(in);
+         passed = expected == out;
+        System.out.println("180 rubles are 3. Test result : " + passed);
     }
 }
