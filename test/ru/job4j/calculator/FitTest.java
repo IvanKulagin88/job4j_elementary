17fb11348e6d9f30503f84e3e5bdfa1c7951e52f
Vasya BelovKulagin1987git config --global user.name Vasya <unserfuhrer@list.ru>
2020-06-17 15:40:54 +0300

    2. Тесты для идеальный вес - скорректировано[#301292]

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -12,21 +12,21 @@ import org.junit.Test;
 public class FitTest {
 
     @Test
-    public static double manWeight(double height) {
+    public void  manWeight(double height) {
         short in = 187;
         double expected = 100.05;
         double out = Fit.manWeight(in);
         Assert.assertEquals(expected, out, 0.01);
-return 0;
+
     }
 
     @Test
-    public static double  womanWeight(double height) {
+    public void womanWeight(double height) {
 
         short  in = 187;
         double expected = 100.05;
         double out = Fit.womanWeight(in);
         Assert.assertEquals(expected, out, 0.01);
-return 0;
+
     }
-}
+    }
