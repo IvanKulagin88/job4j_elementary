8e87a766953bc1cb0c9ae32898fa87fe0a6fad25
Vasya BelovKulagin1987git config --global user.name Vasya <unserfuhrer@list.ru>
2021-02-26 16:24:28 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#301314]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -1,11 +1,22 @@
+
 package ru.job4j.loop;
 
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
 
-         for (; start<=finish; start++) {
+        for (; start<=finish; start++) {
             sum = sum + start;
+        }
+        return sum;
+    }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (; start<=finish; start++) {
+            if ( start %2 ==0 )
+                sum = sum + start ;
+
         }
         return sum;
     }
@@ -14,5 +25,9 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
