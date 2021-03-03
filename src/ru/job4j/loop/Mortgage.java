d4598dbc3341eda8845e56fce3f6d50354a10c72
Vasya BelovKulagin1987git config --global user.name Vasya <unserfuhrer@list.ru>
2021-03-03 09:47:03 +0300

    5.7. Ипотека. [#301308]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,10 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
-        int year =1;
+        int year =0;
 
 
-        while ((amount+amount*percent*0.01) >= salary) {
+        while (amount>0) {
 
            amount = (amount + amount * percent*0.01) - salary;
             year++;
