f68fd77070c912e4110364e7afbd2ad91d0b6481
Vasya BelovKulagin1987git config --global user.name Vasya <unserfuhrer@list.ru>
2021-03-03 16:49:15 +0300

    6.0.2. Заполнение массива. [#301241]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,8 +1,15 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
+    public static void main(String[] args) {
 
-    short [] ages = new short [10];
-    String [] surnames = new String [100500];
-    float [] prices = new float [40];
+
+
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Stepan";
+        names[2] = "Mitrofan";
+        names[3] = "Foma";
+        System.out.println(names);
+    }
 }
